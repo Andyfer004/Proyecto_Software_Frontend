81ae66f61dbfed2dfcb4aff621bb8782fab67b6b
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-05-22 18:38:59 -0600

    modal agregar perfil en sidebar

--- src/common/SidebarGeneral.tsx
+++ src/common/SidebarGeneral.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {  Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigation } from '@react-navigation/native';
 import LoginIcon from '@mui/icons-material/Login';
+import AddCircle from '@mui/icons-material/AddCircle';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -50,6 +51,10 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     { name: 'Login', icon: LoginIcon, route: 'Login' },
   ];
 
+  const bottomDrawerItems = [
+    { name: 'Add', icon: AddCircle , route: 'AddProfile' },
+  ];
+
 const SidebarGeneral = () => {
 
   const [open, setOpen] = React.useState(true);
@@ -69,7 +74,7 @@ const SidebarGeneral = () => {
 
   return (
     <>
-    <AppBar position="fixed" open={open}>
+      <AppBar position="fixed" open={open}>
         <Toolbar>
           <IconButton
             color="inherit"
@@ -87,7 +92,7 @@ const SidebarGeneral = () => {
       </AppBar>
       <Drawer
         sx={{
-          ...( open && {width: drawerWidth}),
+          width: drawerWidth,
           flexShrink: 0,
           '& .MuiDrawer-paper': {
             width: drawerWidth,
@@ -117,9 +122,20 @@ const SidebarGeneral = () => {
           ))}
         </List>
         <Divider />
-        
+        <List sx={{ marginTop: 'auto' }}>
+          {bottomDrawerItems.map((item, index) => (
+            <ListItem key={item.name} disablePadding>
+              <ListItemButton onClick={() => navigation.navigate(item.route as never)}>
+                <ListItemIcon>
+                  {React.createElement(item.icon)}
+                </ListItemIcon>
+                <ListItemText primary={item.name} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
-      </>
+    </>
   );
 };
 
