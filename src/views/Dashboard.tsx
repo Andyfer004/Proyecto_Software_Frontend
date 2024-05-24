449ee968fcac8139173987ae0687f50e30504b8b
davisBold <142925428+davisBold@users.noreply.github.com>
2024-05-24 09:21:00 -0600

    split components dashboard

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -7,27 +7,9 @@ import  {FunctionComponent, ReactNode, useEffect, useState} from 'react'
 import Calendar from './Dashboard/Calendar';
 import GlobalLayout from 'src/common/GlobalLayout';
 import { Box } from '@mui/material';
+import DaySelector from './Dashboard/DaySelector';
 
 
-const DaySelector = () => {
-  return (
-    <div style={{ marginBottom: '20px' }}> {/* Espacio inferior para separarlo del ProgressBar */}
-      <select style={selectStyle}>
-        <option value="Monday">Today</option>
-      </select>
-    </div>
-  );
-};
-
-const selectStyle = {
-  padding: '8px',
-  fontSize: '16px',
-  border: '1px solid #ccc',
-  borderRadius: '4px',
-  backgroundColor: '#f9f9f9',
-  boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.1)',
-  width: '200px', // Ajusta el ancho según sea necesario
-};
 
 
 
---
+++ src/views/Dashboard/DaySelector.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const DaySelector = () => {
+    return (
+        <>
+            <div style={{ marginBottom: '20px' }}> {/* Espacio inferior para separarlo del ProgressBar */}
+                <select style={selectStyle}>
+                <option value="Monday">Today</option>
+                </select>
+            </div>
+      </>
+    );
+  };
+  
+  const selectStyle = {
+    padding: '8px',
+    fontSize: '16px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    backgroundColor: '#f9f9f9',
+    boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.1)',
+    width: '200px', // Ajusta el ancho según sea necesario
+  };
+
+  
+export default DaySelector;
