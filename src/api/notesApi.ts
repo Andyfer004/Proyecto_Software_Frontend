88e6e980ffd01ff5990da95b96ae25e5c4704134
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-07-30 23:17:09 -0600

    Se agregaron las rutas de add create delete get de la api notes del backend

---
+++ src/api/notesApi.ts
@@ -0,0 +1,41 @@
+// src/api/notesApi.ts
+const BASE_URL = 'http://localhost:8000/api';
+
+export const getNotes = async () => {
+  const response = await fetch(`${BASE_URL}/notes`);
+  return response.json();
+};
+
+export const getNote = async (id: number) => {
+  const response = await fetch(`${BASE_URL}/notes/${id}`);
+  return response.json();
+};
+
+export const addNote = async (note: { note: string, image: string, profile_id: number }) => {
+  const response = await fetch(`${BASE_URL}/notes`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
+  return response.json();
+};
+
+export const updateNote = async (id: number, updatedFields: Partial<{ note: string, image: string, profile_id: number }>) => {
+  const response = await fetch(`${BASE_URL}/notes/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedFields),
+  });
+  return response.json();
+};
+
+export const deleteNote = async (id: number) => {
+  const response = await fetch(`${BASE_URL}/notes/${id}`, {
+    method: 'DELETE',
+  });
+  return response.json();
+};
