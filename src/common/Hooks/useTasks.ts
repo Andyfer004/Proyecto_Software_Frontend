95834d3908901b05fc368e7227e9bb902626c438
DiederichSolis <sol22952@uvg.edu.gt>
2024-07-31 23:30:39 -0600

    feat: finalizacion de hook

--- src/common/Hooks/useTasks.ts
+++ src/common/Hooks/useTasks.ts
@@ -10,3 +10,67 @@ type Task = {
   created_at: string;
   updated_at: string;
 };
+
+
+const useTasks = () => {
+    const [data, setData] = useState<Task[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+  
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const tasks = await getTasks();
+        setData(tasks);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
+  
+    const createTask = async (newTask: Omit<Task, 'id' | 'created_at' | 'updated_at'>) => {
+      setLoading(true);
+      try {
+        await addTask(newTask);
+        await fetchData(); // Refrescar la lista de tareas
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    const modifyTask = async (id: number, updatedTask: Partial<Omit<Task, 'id'>>) => {
+      setLoading(true);
+      try {
+        await updateTask(id, updatedTask);
+        await fetchData(); // Refrescar la lista de tareas
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    const removeTask = async (id: number) => {
+      setLoading(true);
+      try {
+        await deleteTask(id);
+        await fetchData(); // Refrescar la lista de tareas
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    return { data, loading, error, createTask, modifyTask, removeTask };
+  };
+  
+  export default useTasks;
+  
