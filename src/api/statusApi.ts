38fb4aef2979771de4306761c6590a19cf364b49
DiederichSolis <sol22952@uvg.edu.gt>
2024-07-31 19:31:58 -0600

    feat: add, update, delete del status

--- src/api/statusApi.ts
+++ src/api/statusApi.ts
@@ -9,3 +9,32 @@ export const getStatus = async (id: number) => {
   const response = await fetch(`${BASE_URL}/status/${id}`);
   return response.json();
 };
+
+export const addStatus = async (status: { name: string }) => {
+  const response = await fetch(`${BASE_URL}/status`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(status),
+  });
+  return response.json();
+};
+
+export const updateStatus = async (id: number, updatedFields: Partial<{ name: string }>) => {
+  const response = await fetch(`${BASE_URL}/status/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedFields),
+  });
+  return response.json();
+};
+
+export const deleteStatus = async (id: number) => {
+  const response = await fetch(`${BASE_URL}/status/${id}`, {
+    method: 'DELETE',
+  });
+  return response.json();
+};
