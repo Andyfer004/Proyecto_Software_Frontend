59e7dd085b1e633245ec80845bbafa028dec0244
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-08-15 01:48:06 -0600

    Se mejoro el diseno del progress circular bar y se agrego animacion al mismo

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -29,38 +29,15 @@ const Dashboard = () => {
               <Reminders/>
             </div>
             <div className='w-100 justify-content-center row'>
-              <ReactCustomizableProgressbar
-                  radius={100}
-                  progress={39}
-                  strokeWidth={20}
-                  strokeColor="#FFC107"
-                  fillColor="none"
-                  strokeLinecap="round"
-                  transition="1s ease-out"
-                  pointerRadius={0}
-                  trackStrokeColor="#e0e0e0"
-                  trackStrokeWidth={10}
-                  rotate={150}
-                  cut={120}
-                  initialAnimation={true}
-                  initialAnimationDelay={500}
-                >
-                  
-                    <div style={{
-                      position: 'absolute',  
-                      top: 0,  
-                      left: 0,  
-                      width: '100%', 
-                      height: '100%',  
-                      display: 'flex', 
-                      justifyContent: 'center', 
-                      alignItems: 'center',  
-                      fontSize: '20px', 
-                      color: '#FFC107'  
-                    }}>
-                      {`${39}%`} 
-                    </div>
-              </ReactCustomizableProgressbar>
+            <ReactCustomizableProgressbar 
+              value={75}
+              valueMax={100}
+              startAngle={-110}
+              endAngle={110}
+              textSize={40}
+              height={200}  
+              gaugeSx={{ color: 'indianred' }}
+            />
             </div>
             <div className='w-100 text-center'>
               <ModalNewTask  />
