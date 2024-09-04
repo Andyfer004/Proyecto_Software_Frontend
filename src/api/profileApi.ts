76caf2ecbbdcc1d8d7e1c3cee101a97c6b1e4e63
DiederichSolis <sol22952@uvg.edu.gt>
2024-09-03 22:00:55 -0600

    feat: creacion del endpoint get por medio del id

--- src/api/profileApi.ts
+++ src/api/profileApi.ts
@@ -4,3 +4,8 @@ export const getProfiles = async () => {
   const response = await fetch(`${BASE_URL}/profiles`);
   return response.json();
 };
+
+export const getProfile = async (id: number) => {
+  const response = await fetch(`${BASE_URL}/profiles/${id}`);
+  return response.json();
+};
