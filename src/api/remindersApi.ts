0065731a1ff34ebfd10264d688528f6e7e3438cd
ebarrera2019263 <ebarrera2019263>
2024-09-06 11:25:28 -0600

    se agrego la conexion reminderes con la api

---
+++ src/api/remindersApi.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'http://localhost:8000';
+
+export const getReminders = async () => {
+  const response = await fetch(`${BASE_URL}/reminders`, {
+    method: 'GET',
+  });
+  return response.json();
+};
