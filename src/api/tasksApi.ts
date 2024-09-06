e7081ca3ee1673409465d61562423808782d29e5
DiederichSolis <sol22952@uvg.edu.gt>
2024-09-06 17:04:09 -0600

    feat; cambios

--- src/api/tasksApi.ts
+++ src/api/tasksApi.ts
@@ -1,40 +1,26 @@
-const BASE_URL = 'http://localhost:8000';
+import api from './index'; // Importa tu instancia de Axios
 
 export const getTasks = async () => {
-  const response = await fetch(`${BASE_URL}/tasks`);
-  return response.json();
+  const response = await api.get('/tasks');
+  return response.data; // Axios devuelve los datos en 'data'
 };
 
 export const getTask = async (id: number) => {
-  const response = await fetch(`${BASE_URL}/tasks/${id}`);
-  return response.json();
+  const response = await api.get(`/tasks/${id}`);
+  return response.data;
 };
 
 export const addTask = async (task: { title: string, description: string, due_date: string, status_id: number }) => {
-  const response = await fetch(`${BASE_URL}/tasks`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(task),
-  });
-  return response.json();
+  const response = await api.post('/tasks', task);
+  return response.data;
 };
 
 export const updateTask = async (id: number, updatedFields: Partial<{ title: string, description: string, due_date: string, status_id: number }>) => {
-  const response = await fetch(`${BASE_URL}/tasks/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedFields),
-  });
-  return response.json();
+  const response = await api.put(`/tasks/${id}`, updatedFields);
+  return response.data;
 };
 
 export const deleteTask = async (id: number) => {
-  const response = await fetch(`${BASE_URL}/tasks/${id}`, {
-    method: 'DELETE',
-  });
-  return response.json();
+  const response = await api.delete(`/tasks/${id}`);
+  return response.data;
 };
