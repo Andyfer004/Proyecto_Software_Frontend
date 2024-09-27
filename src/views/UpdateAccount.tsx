e5f8bba78aebec448260a0cb6ab37ade0a474cda
DiederichSolis <sol22952@uvg.edu.gt>
2024-09-27 16:00:31 -0600

    feat: cambios en el update account

--- src/views/UpdateAccount.tsx
+++ src/views/UpdateAccount.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Card, CardContent, Typography } from "@mui/material";
 
 const UpdateAccountScreen: React.FC = () => {
   const [name, setName] = useState('');
@@ -24,52 +24,58 @@ const UpdateAccountScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Update Account</Text>
-      <View style={styles.form}>
-        <TextField
-          label="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          fullWidth
-          style={styles.input}
-          variant="outlined" // Agrega bordes al TextField
-        />
-        <TextField
-          label="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          fullWidth
-          style={styles.input}
-          variant="outlined"
-        />
-        <TextField
-          label="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          fullWidth
-          style={styles.input}
-          variant="outlined"
-        />
-        <TextField
-          label="Phone"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          type="number"
-          fullWidth
-          style={styles.input}
-          variant="outlined"
-        />
-        <Button 
-          variant="contained"
-          color="primary"
-          onClick={handleUpdate}
-          style={styles.button}
-          fullWidth // Botón ocupa todo el ancho disponible
-        >
-          Update
-        </Button>
-      </View>
+      <Card style={styles.card}>
+        <CardContent>
+          <Typography variant="h5" textAlign={"center"} style={styles.heading}>
+            Update Account
+          </Typography>
+          <View style={styles.form}>
+            <TextField
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              fullWidth
+              style={styles.input}
+              variant="outlined"
+            />
+            <TextField
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              style={styles.input}
+              variant="outlined"
+            />
+            <TextField
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              fullWidth
+              style={styles.input}
+              variant="outlined"
+            />
+            <TextField
+              label="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              type="number"
+              fullWidth
+              style={styles.input}
+              variant="outlined"
+            />
+            <Button 
+              variant="contained"
+              color="primary"
+              onClick={handleUpdate}
+              style={styles.button}
+              fullWidth
+            >
+              Update
+            </Button>
+          </View>
+        </CardContent>
+      </Card>
     </View>
   );
 };
@@ -80,25 +86,27 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
-    backgroundColor: '#f9f9f9', // Fondo más claro
+    backgroundColor: '#f9f9f9',
+  },
+  card: {
+    width: '100%',
+    maxWidth: 500, // Ancho máximo de la tarjeta
+    padding: 20,
+    borderRadius: 30,
   },
   heading: {
-    fontSize: 24,
-    fontWeight: "bold",
     marginBottom: 20,
-    color: '#333', // Color de texto más oscuro
+    color: '#333',
   },
   form: {
-    width: '100%', // Asegúrate de que el formulario ocupe el ancho completo
-    maxWidth: 400, // Ancho máximo para mantener el diseño
+    width: '100%',
   },
   input: {
-    marginBottom: 15, // Aumenta el margen entre los campos
+    marginBottom: 15,
   },
   button: {
     marginTop: 20,
   },
-  
 });
 
 export default UpdateAccountScreen;
