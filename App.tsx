afedf02e8b21a80cc6f13535fdfe907d21cfc96e
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2024-11-01 01:57:15 -0600

    Se cambio logo

--- App.tsx
+++ App.tsx
@@ -24,11 +24,13 @@ const WebNavigation = () => (
       {/* Rutas no protegidas */}
       <Route path="/login" element={<LoginScreen />} />
       
-
+      {/* Rutas protegidas por el componente ProtectedRoute */}
+      <Route element={<ProtectedRoute />}>
         <Route path="/home" element={<Dashboard />} />
         <Route path="/update-account" element={<UpdateAccountScreen />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="*" element={<Navigate to="/home" />} />
+      </Route>
 
     </Routes>
   </Router>
